0ca25e59a5471f68161cf1faa13b73d8f506add7
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-16 14:41:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function volume_sphere() {
     //Write your code here
 	var rad = document.getElementById("radius").value;
+	rad = Math.abs(rad);
 	var val = (3/4) * Math.PI * Math.pow(rad, 3);
+	val = val.toFixed(4);
 	document.getElementById("volume").value = val;
 	return false;
 } 
